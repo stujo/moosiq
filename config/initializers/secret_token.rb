7e835a0e22698a6cba9db89383c8e6096da9433f
stujo <stujo@imagescript.com>
2014-04-23 09:37:16 -0700

    rails new moosiq --skip-test-unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Moosiq::Application.config.secret_key_base = 'd2c04a48ce4d068fb3eeac19cc18e8c91de484d8e0d830f882a9cbcc18339daf770ca111d8ef31e565aff05ac54fcf16c49efc1b93e50c3ec54c5c3669febb90'
