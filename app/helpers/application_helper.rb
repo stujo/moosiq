3cd0375d267857900c41b9c317fe18014460404a
stujo <stujo@imagescript.com>
2014-04-24 10:57:15 -0700

    Basic Layout Fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def main_menu_items active_tag
+    [
+        {tag: "agents", label: "Agents", path: agents_path, active: false},
+        {tag: "bands", label: "Bands", path: bands_path, active: false},
+        {tag: "albums", label: "Albums", path: albums_path, active: false},
+        {tag: "tracks", label: "Tracks", path: tracks_path, active: false}
+    ].each do |item|
+      item[:active] = item[:tag] == active_tag
+    end
+  end
 end
