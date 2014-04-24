c189136fa6d7a2925e5474e550591c4b6662cb7a
stujo <stujo@imagescript.com>
2014-04-24 14:57:36 -0700

    simple_form checkboxes association on album for for tracks

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -53,6 +53,6 @@ class AlbumsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def album_params
-      params.require(:album).permit(:title, :released_on)
+      params.require(:album).permit(:title, :released_on, :track_ids => [])
     end
 end
