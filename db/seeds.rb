2850ce61f797637b1f783f9a835ee4942e31cef1
stujo <stujo@imagescript.com>
2014-04-24 15:12:05 -0700

    Updating Seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,7 @@ if Object.const_get('Agent')
   bob = seeder Agent, :first_name, {first_name: 'Bob', last_name: 'Williams', phone_number: '(415)-555-5556'}
   john = seeder Agent, :first_name, {first_name: 'John', last_name: 'Smith', phone_number: '(415)-555-6666'}
   walter = seeder Agent, :first_name, {first_name: 'Walter', last_name: 'White', phone_number: '(415)-555-7777'}
+  paul = seeder Agent, :first_name, {first_name: 'Paul', last_name: 'Jones', phone_number: '(415)-555-8888'}
 
 
   if Object.const_get('Band')
@@ -23,10 +24,14 @@ if Object.const_get('Agent')
       goldstar = seeder Track, :title, {title: 'GoldSTAR', released_on: Date.parse('2012-02-03'), seconds: 291, band: beafles}
       silverstar = seeder Track, :title, {title: 'SilverMoon', released_on: Date.parse('2011-01-12'), seconds: 302, band: beafles}
       spades = seeder Track, :title, {title: 'Ace of Spades', released_on: Date.parse('2010-04-04'), seconds: 293, band: aces}
+      muffled = seeder Track, :title, {title: 'Muffled', released_on: Date.parse('2009-05-05'), seconds: 201, band: beanies}
       nutty = seeder Track, :title, {title: 'Nutty Jam', released_on: Date.parse('2009-05-05'), seconds: 182, band: walnuts}
+      hard_to_hear  = seeder Track, :title, {title: 'Hard to Hear', released_on: Date.parse('2011-01-07'), seconds: 306, band: aces}
+      big_thing = seeder Track, :title, {title: 'Next Big Thing', released_on: Date.parse('2012-02-03'), seconds: 291, band: beafles}
 
       if Object.const_get('Album')
-        stars = seeder Album, :title, {title: 'Stars', released_on: Date.parse('2013-02-03'), tracks: [goldstar, silverstar]}
+        stars = seeder Album, :title, {title: 'Stars', released_on: Date.parse('2013-02-03'), tracks: [goldstar, silverstar,spades]}
+        stars = seeder Album, :title, {title: 'Silent Movie Soundtrack', released_on: Date.parse('2014-01-03'), tracks: [muffled, hard_to_hear, big_thing,nutty]}
       end
     end
   end
